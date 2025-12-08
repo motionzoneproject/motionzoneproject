e6907f8fa5ada02a808258de359423203458f4a1
Ahmed-Sayid <twakal222@gmail.com>
2025-12-08 14:27:26 +0100

    header changing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 export default function Page() {
   return (
-    <div className="w-full min-h-1/3">
-      <div className="mx-auto text-center mt-10">
-        <div className="animate-bounce text-6xl">💃🏻🕺🏽</div> MotionZone
-        dansstudio
+    <div className="w-full bg-slate-200 min-h-screen">
+      <div className="mx-auto text-center mt-0">
+        <div className="animate-bounce text-3xl text-black font-extrabold">
+          💃🏻🕺🏽
+          <h1>MotionZone dansstudio</h1>
+        </div>
         <br />
         <div className="mx-auto w-20 h-20 rounded-full border-8 border-purple-500 border-t-pink-400 animate-spin"></div>
       </div>
