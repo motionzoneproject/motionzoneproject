d9f2f48fb24f73dd14835d173fda7b837bfe12ba
Ahmed-Sayid <twakal222@gmail.com>
2025-12-08 14:47:52 +0100

    signin

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,11 @@
+
+export default function SignInPage(){
+
+
+    return (
+
+        <div className=" w-full flex justify-center  items-center p-4 min-h-screen bg-slate-400">
+            <h1 className="text-6xl font-bold">Sign In</h1>
+        </div>
+    )
+}
