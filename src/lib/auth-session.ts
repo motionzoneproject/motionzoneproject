c91523dc994af05e2d6751ce71f2a6818913bdd9
Ahmed-Sayid <twakal222@gmail.com>
2025-12-10 12:50:18 +0100

    auth

--- src/lib/auth-session.ts
+++ src/lib/auth-session.ts
@@ -1,23 +1,23 @@
-type BetterAuthSession = {
-  session: {
-    session: {
-      id: string;
-      createdAt: Date;
-      updatedAt: Date;
-      userId: string;
-      expiresAt: Date;
-      token: string;
-      ipAddress?: string | null | undefined;
-      userAgent?: string | null | undefined;
-    };
-    user: {
-      id: string;
-      createdAt: Date;
-      updatedAt: Date;
-      email: string;
-      emailVerified: boolean;
-      name: string;
-      image?: string | null | undefined;
-    };
-  } | null;
-};
+// type BetterAuthSession = {
+//   session: {
+//     session: {
+//       id: string;
+//       createdAt: Date;
+//       updatedAt: Date;
+//       userId: string;
+//       expiresAt: Date;
+//       token: string;
+//       ipAddress?: string | null | undefined;
+//       userAgent?: string | null | undefined;
+//     };
+//     user: {
+//       id: string;
+//       createdAt: Date;
+//       updatedAt: Date;
+//       email: string;
+//       emailVerified: boolean;
+//       name: string;
+//       image?: string | null | undefined;
+//     };
+//   } | null;
+// };
