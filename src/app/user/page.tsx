aed334b9752471f332f5948494ad84db88289c19
Tobias Eriksson <hungatobbe@gmail.com>
2025-12-18 14:30:00 +0100

    Lagt till en kalender som visar alla kurstillfällen kunden kan boka, samt sina bokningar

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,3 +1,38 @@
-export default function Page() {
-  return <div>Profilsida för student. Här sker bokningar osv.</div>;
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { getUserBookings, getUserLessons } from "@/lib/actions/server-actions";
+import { getSessionData } from "@/lib/actions/sessiondata";
+import BookingCal from "./BookingCal";
+
+export default async function Page() {
+  const sessionData = await getSessionData();
+  const user = sessionData?.user;
+
+  const { lessons = [] } = await getUserLessons();
+  const { bookings = [] } = await getUserBookings();
+
+  return (
+    <div>
+      <Card>
+        <CardHeader>
+          <CardTitle>{user?.name} - Profilsida</CardTitle>
+          <CardDescription>
+            Här kan du boka lektioner från dina köpta produkter, och ändra dina
+            uppgifter.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          (BOKA)
+          <BookingCal lessons={lessons} bookings={bookings} />
+          <br />
+          (Uppgifter)
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
