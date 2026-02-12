e568840d8f3cf2c4a78c55ea5d2efa3d56e49a33
Tobias Eriksson <hungatobbe@gmail.com>
2026-02-12 14:11:57 +0100

    layout and side-panel fix

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,4 +1,5 @@
 import {
+  BookOpen,
   CalendarDays,
   GraduationCap,
   Home,
@@ -21,64 +22,80 @@ import {
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
 import { isAdminRole } from "@/lib/actions/admin";
-
-// Menu items.
-const items = [
-  {
-    title: "Översikt",
-    url: "/admin",
-    icon: Home,
-  },
-  {
-    title: "Startsida",
-    url: "/admin/start",
-    icon: Home,
-  },
-  {
-    title: "Om oss",
-    url: "/admin/omoss",
-    icon: Sparkles,
-  },
-  {
-    title: "Kurser",
-    url: "/admin/courses",
-    icon: GraduationCap,
-  },
-  {
-    title: "Produkter",
-    url: "/admin/products",
-    icon: Package,
-  },
-  {
-    title: "Ordrar",
-    url: "/admin/orders",
-    icon: ShoppingCart,
-  },
-  {
-    title: "Event",
-    url: "/admin/events",
-    icon: CalendarDays,
-  },
-  {
-    title: "Galleri",
-    url: "/admin/gallery",
-    icon: ImageIcon,
-  },
-  {
-    title: "Elever",
-    url: "/admin/students",
-    icon: Users,
-  },
-  {
-    title: "Terminer / Scheman",
-    url: "/admin/termin",
-    icon: CalendarDays,
-  },
-];
+import { getSessionData } from "@/lib/actions/sessiondata";
 
 export async function AppSidebar() {
   const isAdmin = await isAdminRole();
   if (!isAdmin) return notFound();
+
+  const sessionData = await getSessionData();
+  const user = sessionData?.user;
+
+  const formatDate = (date: Date) => date.toISOString().split("T")[0];
+  const today = new Date();
+  const in7 = new Date();
+  in7.setDate(in7.getDate() + 7);
+  const from = formatDate(today);
+  const to = formatDate(in7);
+
+  // Menu items.
+  const items = [
+    {
+      title: "Översikt",
+      url: "/admin",
+      icon: Home,
+    },
+    {
+      title: "Lektioner",
+      url: `/admin/lectures?teacher=${user?.id}&from=${from}&to=${to}`,
+      icon: BookOpen,
+    },
+    {
+      title: "Startsida",
+      url: "/admin/start",
+      icon: Home,
+    },
+    {
+      title: "Om oss",
+      url: "/admin/omoss",
+      icon: Sparkles,
+    },
+    {
+      title: "Kurser",
+      url: "/admin/courses",
+      icon: GraduationCap,
+    },
+    {
+      title: "Produkter",
+      url: "/admin/products",
+      icon: Package,
+    },
+    {
+      title: "Ordrar",
+      url: "/admin/orders",
+      icon: ShoppingCart,
+    },
+    {
+      title: "Event",
+      url: "/admin/events",
+      icon: CalendarDays,
+    },
+    {
+      title: "Galleri",
+      url: "/admin/gallery",
+      icon: ImageIcon,
+    },
+    {
+      title: "Elever",
+      url: "/admin/students",
+      icon: Users,
+    },
+    {
+      title: "Terminer / Scheman",
+      url: "/admin/termin",
+      icon: CalendarDays,
+    },
+  ];
 
   return (
     <Sidebar>
