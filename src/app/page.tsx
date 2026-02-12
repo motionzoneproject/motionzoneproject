82818fed6bd1dca7aa8d011cbc6469df5baefeef
Ahmed-Sayid <twakal222@gmail.com>
2026-02-13 00:40:29 +0100

    80 startsida event (#81)
    * event fixes
    
    * event image
    
    ---------
    
    Co-authored-by: Alexander Sjösten <alexander.sjosten@lexicon.se>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,13 @@ import Features from "./start/Features";
 import Hero from "./start/Hero";
 
 export default async function Page() {
-  const _events = await prisma.event.findMany();
+  const events = await prisma.event.findMany();
 
   return (
     <main className="flex-1 bg-background">
       <Hero />
       <Features />
-      <Events events={[]} />
+      <Events events={events} />
     </main>
   );
 }
