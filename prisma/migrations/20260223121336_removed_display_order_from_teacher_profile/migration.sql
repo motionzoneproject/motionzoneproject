18b5b1878e600588437ff15e70065df024c1cab2
Tobias Eriksson <hungatobbe@gmail.com>
2026-02-23 13:15:44 +0100

    Removed displayorder from schema

---
+++ prisma/migrations/20260223121336_removed_display_order_from_teacher_profile/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `displayOrder` on the `teacher_profile` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "teacher_profile" DROP COLUMN "displayOrder";
