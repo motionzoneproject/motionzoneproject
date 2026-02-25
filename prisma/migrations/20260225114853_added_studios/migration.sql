c04ec268325beca6a982b8850e403cfb48c82944
Tobias Eriksson <hungatobbe@gmail.com>
2026-02-25 12:51:51 +0100

    Added studio in prismaschema

---
+++ prisma/migrations/20260225114853_added_studios/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Studios" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "imageUrl" TEXT,
+
+    CONSTRAINT "Studios_pkey" PRIMARY KEY ("id")
+);
